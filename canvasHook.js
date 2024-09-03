4dee90250bcfdcb896edecb81cc8924faf5759cc
eason8811 <3220563569@qq.com>
2024-09-03 16:30:28 +0800

    1

--- canvasHook.js
+++ canvasHook.js
@@ -1,83 +1,130 @@
-// 保存原始的 translate 和 scale 方法
-const canvasNode = document.getElementsByClassName('chart-gui-wrapper')[0].children[0];
-const ctx = canvasNode.getContext('2d');
-const originalFillRect = ctx.fillRect;
+// 保存原始的 fillRect 方法
+const canvasNode1 = document.getElementsByClassName('chart-gui-wrapper')[0].children[0];
+const ctx1 = canvasNode1.getContext('2d');
+const originalFillRect1 = ctx1.fillRect;
+
+const canvasNode2 = document.getElementsByClassName('chart-gui-wrapper')[0].children[1];
+const ctx2 = canvasNode2.getContext('2d');
+const originalFillRect2 = ctx2.fillRect;
+
 window.toolsItem = {
     profitY: null,
     stopY: null
 };
 
+function draw(ctx, x, y, width, height, enterButton, radius) {
+    // 绘制下单按钮
+    ctx.save();
+
+    if (window.toolsItem['stopY'] >= window.toolsItem['profitY']) {
+        // 代表当前是多头工具
+        ctx.fillStyle = 'white';
+        // originalFillRect1.call(ctx, x + width, window.toolsItem['stopY'], enterButton['width'], enterButton['height']);
+        x = x + width;
+        y = window.toolsItem['stopY'];
+        width = 200;
+        height = 100;
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);  // 从矩形左上角开始
+        ctx.lineTo(x + width - radius, y); // 上边
+        ctx.arcTo(x + width, y, x + width, y + radius, radius); // 右上角
+        ctx.lineTo(x + width, y + height - radius); // 右边
+        ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius); // 右下角
+        ctx.lineTo(x + radius, y + height); // 下边
+        ctx.arcTo(x, y + height, x, y + height - radius, radius); // 左下角
+        ctx.lineTo(x, y + radius); // 左边
+        ctx.arcTo(x, y, x + radius, y, radius); // 左上角
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    } else {
+        // 代表当前是空头工具
+        ctx.fillStyle = 'red';
+        // originalFillRect1.call(ctx, x + width, y, enterButton['width'], enterButton['height']);
+        x = x + width;
+        width = 200;
+        height = 100;
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);  // 从矩形左上角开始
+        ctx.lineTo(x + width - radius, y); // 上边
+        ctx.arcTo(x + width, y, x + width, y + radius, radius); // 右上角
+        ctx.lineTo(x + width, y + height - radius); // 右边
+        ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius); // 右下角
+        ctx.lineTo(x + radius, y + height); // 下边
+        ctx.arcTo(x, y + height, x, y + height - radius, radius); // 左下角
+        ctx.lineTo(x, y + radius); // 左边
+        ctx.arcTo(x, y, x + radius, y, radius); // 左上角
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    window.toolsItem['stopY'] = null;
+    window.toolsItem['profitY'] = null;
+    ctx.restore();
+}
 
 // 覆盖 fillRect 方法
-ctx.fillRect = function (x, y, width, height) {
+ctx1.fillRect = function (x, y, width, height) {
     let enterButton = {
         height: Math.min(Math.max(15, 0.5 * height), 100),
         width: Math.min(Math.max(40, 0.3 * width), 200)
     }
-    if (ctx.fillStyle === window.profitColor || ctx.fillStyle === window.stopColor){
-        console.log(`fillRect x: ${x}, y: ${y}, width: ${width}, height: ${height} color: ${ctx.fillStyle}`);
-    }
 
-    if (window.longToolColor && ctx.fillStyle === window.longToolColor.stopColor) {
-        console.log(`LongStop x: ${x}, y: ${y}, width: ${width}, height: ${height} color: ${ctx.fillStyle}`);
-        window.toolsItem['stopY'] = y;
-
-        // 调用原始的 scale 方法
-        originalFillRect.call(this, x, y, width, height);
-    } else if (window.longToolColor && ctx.fillStyle === window.longToolColor.profitColor && window.toolsItem['stopY']) {
-        console.log(`LongProfit x: ${x}, y: ${y}, width: ${width}, height: ${height} color: ${ctx.fillStyle}`);
+    // 颜色识别当前为多头工具的止损
+    if (window.longToolColor && ctx1.fillStyle === window.longToolColor.stopColor) {
+        window.toolsItem['stopY'] = y;                      // 记录止损的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        // 颜色识别当前为多头工具的止盈
+    } else if (window.longToolColor && ctx1.fillStyle === window.longToolColor.profitColor && window.toolsItem['stopY']) {
+        window.toolsItem['profitY'] = y;                    // 记录止盈的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        draw(ctx1, x, y, width, height, enterButton, 20);        // 绘制下单按钮
+        // 颜色识别当前为空头工具的止损
+    } else if (window.shortToolColor && ctx1.fillStyle === window.shortToolColor.stopColor) {
+        window.toolsItem['stopY'] = y;                      // 记录止损的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        // 颜色识别当前为空头工具的止盈
+    } else if (window.shortToolColor && ctx1.fillStyle === window.shortToolColor.profitColor && window.toolsItem['stopY']) {
         window.toolsItem['profitY'] = y;
-
-        // 调用原始的 scale 方法
-        originalFillRect.call(this, x, y, width, height);
-        ctx.save();
-
-        if (window.toolsItem['stopY'] >= window.toolsItem['profitY']) {
-            // 代表当前是多头工具
-            ctx.fillStyle = 'white';
-            originalFillRect.call(this, x + width, window.toolsItem['stopY'], enterButton['width'], enterButton['height']);
-        } else {
-            // 代表当前是空头工具
-            ctx.fillStyle = 'red';
-            originalFillRect.call(this, x + width, y, enterButton['width'], enterButton['height']);
-        }
-
-        window.toolsItem['stopY'] = null;
-        window.toolsItem['profitY'] = null;
-        ctx.restore();
-    } else if (window.shortToolColor && ctx.fillStyle === window.shortToolColor.stopColor) {
-        // console.log(`ShortStop x: ${x}, y: ${y}, width: ${width}, height: ${height} color: ${ctx.fillStyle}`);
-        window.toolsItem['stopY'] = y;
-
-        // 调用原始的 scale 方法
-        originalFillRect.call(this, x, y, width, height);
-    } else if (window.shortToolColor && ctx.fillStyle === window.shortToolColor.profitColor && window.toolsItem['stopY']) {
-        // console.log(`ShortProfit x: ${x}, y: ${y}, width: ${width}, height: ${height} color: ${ctx.fillStyle}`);
-        window.toolsItem['profitY'] = y;
-
-        // 调用原始的 scale 方法
-        originalFillRect.call(this, x, y, width, height);
-        ctx.save();
-
-        if (window.toolsItem['stopY'] >= window.toolsItem['profitY']) {
-            // 代表当前是多头工具
-            ctx.fillStyle = 'white';
-            originalFillRect.call(this, x + width, window.toolsItem['stopY'], enterButton['width'], enterButton['height']);
-        } else {
-            // 代表当前是空头工具
-            ctx.fillStyle = 'red';
-            originalFillRect.call(this, x + width, y, enterButton['width'], enterButton['height']);
-        }
-
-        window.toolsItem['stopY'] = null;
-        window.toolsItem['profitY'] = null;
-        ctx.restore();
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        draw(ctx1, x, y, width, height, enterButton, 20);        // 绘制下单按钮
     } else {
-        originalFillRect.call(this, x, y, width, height);
+        originalFillRect1.call(this, x, y, width, height);
+    }
+};
+
+// 覆盖 fillRect 方法
+ctx2.fillRect = function (x, y, width, height) {
+    let enterButton = {
+        height: Math.min(Math.max(15, 0.5 * height), 100),
+        width: Math.min(Math.max(40, 0.3 * width), 200)
     }
 
+    // 颜色识别当前为多头工具的止损
+    if (window.longToolColor && ctx2.fillStyle === window.longToolColor.stopColor) {
+        window.toolsItem['stopY'] = y;                      // 记录止损的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        // 颜色识别当前为多头工具的止盈
+    } else if (window.longToolColor && ctx2.fillStyle === window.longToolColor.profitColor && window.toolsItem['stopY']) {
+        window.toolsItem['profitY'] = y;                    // 记录止盈的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        draw(ctx2, x, y, width, height, enterButton, 20);        // 绘制下单按钮
+        // 颜色识别当前为空头工具的止损
+    } else if (window.shortToolColor && ctx2.fillStyle === window.shortToolColor.stopColor) {
+        window.toolsItem['stopY'] = y;                      // 记录止损的绘图位置
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        // 颜色识别当前为空头工具的止盈
+    } else if (window.shortToolColor && ctx2.fillStyle === window.shortToolColor.profitColor && window.toolsItem['stopY']) {
+        window.toolsItem['profitY'] = y;
+        originalFillRect1.call(this, x, y, width, height);  // 调用原始的 fillRect 方法
+        draw(ctx2, x, y, width, height, enterButton, 20);        // 绘制下单按钮
+    } else {
+        originalFillRect1.call(this, x, y, width, height);
+    }
 };
 
 // // 还原 fillRect 方法
-// ctx.fillRect = originalFillRect;
+// ctx1.fillRect = originalFillRect1;
+// ctx2.fillRect = originalFillRect2;
 
