624116594b1065dbca56a6de646fcdcfc9128ed1
eason8811 <3220563569@qq.com>
2024-10-10 16:41:25 +0800

    重构代码

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     } else {
         sendResponse({response: `${request.key},${request.value} content.js已收到`});  // 可选的，返回响应给 background.js
         const event = new CustomEvent(request.key, {
-            detail:{
+            detail: {
                 key: request.key,
                 value: request.value
             }
@@ -44,22 +44,25 @@ function injectScript(file) {
     console.log(`${file} 脚本开始加载`);
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.id = 'fetchHook';
+    script.id = file.split('/')[1].replace('.js', '');
     script.src = chrome.runtime.getURL(file);  // 获取插件内的脚本文件的绝对路径
     console.log(script);
     script.onload = function () {
-        // 在脚本加载完成后，可以选择移除它，防止污染 DOM
         // this.remove();  // 这里选择在加载完后移除 <script> 标签
         console.log(`${file} 脚本加载完成`);
     };
     (document.head || document.documentElement).appendChild(script);  // 将脚本插入页面
+    return script;
 }
 
 // 注入脚本
-injectScript('functionScripts/webpackHook.js');
-injectScript('functionScripts/adsBlock.js');
-injectScript('functionScripts/buttonDraw.js');
-injectScript('functionScripts/alertFunc.js');
+let webpackHookScript = injectScript('functionScripts/webpackHook.js');
+webpackHookScript.onload = () => {
+    injectScript('functionScripts/adsBlock.js');
+    injectScript('functionScripts/buttonDraw.js');
+    injectScript('functionScripts/alertFunc.js');
+};
+
 
 
 
--- functionScripts/alertFunc.js
+++ functionScripts/alertFunc.js
@@ -1,4 +1,12 @@
+window.ExpireType = {
+    'expire': true,
+    'infinite': false
+}
+window.currentAlertExpireType = window.ExpireType.expire;
+
 (function listenAlertSet() {
+    let originExpireDate;
+
     function activateOriginAlertSetter(event) {
         let pageList = document.querySelectorAll('div[class="widgetbar-pagescontent"] > div');
         let selectedPageIndex = -1;
@@ -10,9 +18,9 @@
         }
         let openAlertPage = document.querySelector('button[aria-label="警报"]');
 
-        const observer = new MutationObserver((mutationsList, observer) => {
+        function openSetterPage(mutationsList, observer) {
             for (const mutation of mutationsList) {
-                if (mutation.target.classList.value === "widgetbar-widgetbody" && mutation.addedNodes[0].classList.value === "wrapper-G90Hl2iS") {
+                if (mutation.target.classList.value === "widgetbar-widgetbody" && mutation.addedNodes.length > 0 && mutation.addedNodes[0].classList.value === "wrapper-G90Hl2iS") {
                     let setAlertButton = document.querySelector('div[data-name="set-alert-button"]');
                     setAlertButton.click();
                     openAlertPage.click();
@@ -23,23 +31,88 @@
                     break;
                 }
             }
+        }
+
+        function changeExpireType(event) {
+            const switcherObserver = new MutationObserver((mutationsList, observer) => {
+
+                switcherObserver.disconnect();
+                let expireTypeSwitcher = document.querySelector('span[class="switcher-fwE97QDf"]');
+                let setExpireDateButtonList = document.querySelector('span[class="switcher-fwE97QDf"]').parentElement.parentElement.querySelectorAll('button[data-day], button[data-overflow-tooltip-text]');
+                let expireTypeSwitcherInnerHTML = expireTypeSwitcher.innerHTML;
+                if (event.type === 'click') {
+                    event.stopPropagation();
+                    originExpireDate = document.querySelector('span[class="content-H6_2ZGVv"]').innerText !== '无限制警报' ? document.querySelector('span[class="content-H6_2ZGVv"]').innerText : originExpireDate;
+                    if (!window.currentAlertExpireType) {
+                        expireTypeSwitcher.innerHTML = expireTypeSwitcherInnerHTML
+                            .replace('switchView-CtnpmPzP small-CtnpmPzP', 'switchView-CtnpmPzP small-CtnpmPzP checked-CtnpmPzP')
+                            .replace('input-fwE97QDf', 'input-fwE97QDf checked-fwE97QDf');
+                    } else {
+                        expireTypeSwitcher.innerHTML = expireTypeSwitcherInnerHTML;
+                    }
+                    expireTypeSwitcher.addEventListener('click', () => {
+                        window.currentAlertExpireType = !window.currentAlertExpireType;
+                        expireTypeSwitcher.querySelector('input').classList.toggle('checked-fwE97QDf');
+                        expireTypeSwitcher.querySelector('span > span > span').classList.toggle('checked-CtnpmPzP');
+                        let expireDateContent = document.querySelector('button[aria-controls="alert-editor-expiration-popup"] > span[class="content-H6_2ZGVv"]');
+                        if (!window.currentAlertExpireType) {
+                            expireDateContent.innerText = '无限制警报';
+                        } else {
+                            expireDateContent.innerText = originExpireDate;
+                        }
+                    });
+
+                    for (let setExpireDateButton of setExpireDateButtonList) {
+                        setExpireDateButton.addEventListener('click', () => {
+                            window.currentAlertExpireType = window.ExpireType.expire;
+                            expireTypeSwitcher.querySelector('input').classList.toggle('checked-fwE97QDf', false);
+                            expireTypeSwitcher.querySelector('span > span > span').classList.toggle('checked-CtnpmPzP', false);
+                            let expireDateContent = document.querySelector('button[aria-controls="alert-editor-expiration-popup"] > span[class="content-H6_2ZGVv"]');
+                            if (setExpireDateButton === document.querySelector('button[class="btn-fpDXgGC1 button-D4RPB3ZC medium-D4RPB3ZC black-D4RPB3ZC primary-D4RPB3ZC apply-overflow-tooltip apply-overflow-tooltip--check-children-recursively apply-overflow-tooltip--allow-text apply-common-tooltip"]')) {
+                                expireDateContent.innerText = document.querySelector('button[class="btn-fpDXgGC1 button-D4RPB3ZC medium-D4RPB3ZC black-D4RPB3ZC primary-D4RPB3ZC apply-overflow-tooltip apply-overflow-tooltip--check-children-recursively apply-overflow-tooltip--allow-text apply-common-tooltip"] > span[class="content-D4RPB3ZC"]').innerText;
+                            }else {
+                                expireDateContent.innerText = originExpireDate;
+                            }
+                        });
+                    }
+                }
+            });
+            let switcherObserverNode = document.querySelector('div[data-name="alerts-create-edit-dialog"] + div');
+            switcherObserver.observe(switcherObserverNode, {childList: true, subtree: true});
+        }
+
+        const observer = new MutationObserver((mutationsList, observer) => {
+            const innerObserver = new MutationObserver((innerMutationsList, innerObserver) => {
+                originExpireDate = document.querySelector('span[class="content-H6_2ZGVv"]').innerText;
+                document.querySelector('button[aria-controls="alert-editor-expiration-popup"]').addEventListener('click', changeExpireType);
+                innerObserver.disconnect();
+            });
+            let innerObserverNode = document.querySelector('#overlap-manager-root');
+            innerObserver.observe(innerObserverNode, {childList: true, subtree: true});
+            openSetterPage(mutationsList, observer);
         });
+
         let observeNode = document.querySelector('div[class="widgetbar-pagescontent"]');
         observer.observe(observeNode, {childList: true, subtree: true});
-        openAlertPage.click();
+        if (selectedPageIndex === 1) {
+            document.querySelector('div[data-name="set-alert-button"]').click();
+        } else {
+            openAlertPage.click();
+        }
     }
 
     window.addEventListener('setAlert', activateOriginAlertSetter);
 })();
 
 class Alert {
-    constructor(symbol, condition, triggerCondition, expire, alertName, alertMessage) {
+    constructor(symbol, condition, triggerCondition, expire, alertName, alertMessage, noteInfo) {
         this.symbol = symbol;
         this.condition = condition;
         this.triggerCondition = triggerCondition;
         this.expire = expire;
         this.alertName = alertName;
         this.alertMessage = alertMessage;
+        this.noteInfo = noteInfo;
     }
 
     Condition = class {
@@ -59,5 +132,10 @@ class Alert {
         }
     }
 
-
+    Expire = class {
+        constructor(expireType, expireDate) {
+            this.expireType = expireType;
+            this.expireDate = expireDate;
+        }
+    }
 }
